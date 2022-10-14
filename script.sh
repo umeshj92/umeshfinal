e5abc5721cb90d1c6b7d2a6a86d8d0047f2c554e
umeshj92 <95260748+umeshj92@users.noreply.github.com>
2022-10-14 13:53:50 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,16 +1,1 @@
-#! /bin/bash
-
-# Given string 
-string="Lelouch,Akame,Kakashi,Wrath"
-
-# Setting IFS (input field separator) value as ","
-IFS=','
-
-# Reading the split string into array
-read -ra arr <<< "$string"
-
-# Print each value of the array by using the loop
-for val in "${arr[@]}";
-do
-  printf "name = $val\n"
-done
+aws --version
